8256b421736ad7764940d7cb4f36c73efddc073b
Igor Suvorov <suekandig@mail.ru>
2023-01-27 18:36:40 +0300

    Добавил приватное свойство Line

--- EncryptionLibrary/Atbash.cs
+++ EncryptionLibrary/Atbash.cs
@@ -8,6 +8,8 @@ namespace EncryptionLibrary
 {
     public class Atbash
     {
+        private string Line { get; set; }
+
         private char[] masAlphavit = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
                                'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
                                's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
