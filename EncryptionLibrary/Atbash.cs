0c59748a26c7ad150f673bfdac941fbce462b93b
Igor Suvorov <suekandig@mail.ru>
2023-01-29 17:42:33 +0300

    Перенес в абстрактный класс Encryption поле arrayAlphavit с латинским алфавитом

--- EncryptionLibrary/Atbash.cs
+++ EncryptionLibrary/Atbash.cs
@@ -8,10 +8,6 @@ namespace EncryptionLibrary
 {
     public class Atbash : Encryption
     {
-        private readonly char[] arrayAlphavit = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-                                                'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-                                                's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-
         public Atbash(string line) => Line = line;
 
         public override string Encrypt()
