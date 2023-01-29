8c1df8b1c1bec35cc6b31d7e249054d64bdfb6a3
Igor Suvorov <suekandig@mail.ru>
2023-01-29 17:21:54 +0300

    К классу Atbash: добавил наследование от Encryption и добавил префикс override для преопределения методов Decrypt() и Encrypt()

--- EncryptionLibrary/Atbash.cs
+++ EncryptionLibrary/Atbash.cs
@@ -6,17 +6,15 @@ using System.Threading.Tasks;
 
 namespace EncryptionLibrary
 {
-    public class Atbash
+    public class Atbash : Encryption
     {
-        private string Line { get; set; }
-
         private readonly char[] masAlphavit = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
                                                 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
                                                 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
         public Atbash(string line) => Line = line;
 
-        public string Encrypt()
+        public override string Encrypt()
         {
             var keys = GetMassiveKeys();
             Reverse();
@@ -32,7 +30,7 @@ namespace EncryptionLibrary
             return result;
         }
 
-        public string Decrypt()
+        public override string Decrypt()
         {
             var keys = GetMassiveKeys();
 
