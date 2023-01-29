710177a299f2fc120ca90ac5fb0a372d78faf493
Igor Suvorov <suekandig@mail.ru>
2023-01-29 17:02:56 +0300

    Реализовал абстрактный класс Encryption, наследованный от интерфейса IEncryption

---
+++ EncryptionLibrary/Abstractions/Encryption.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EncryptionLibrary.Interfaces;
+
+namespace EncryptionLibrary.Abstractions
+{
+    abstract class Encryption : IEncryption
+    {
+        private string Line { get; set; }
+
+        public Encryption(string line) => Line = line;
+
+        public abstract string Encrypt();
+        public abstract string Decrypt();
+    }
+}
