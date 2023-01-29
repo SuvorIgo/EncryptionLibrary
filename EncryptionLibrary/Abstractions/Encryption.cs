abac9bd28a1c6764c03f52ba0729c4f453ff77d9
Igor Suvorov <suekandig@mail.ru>
2023-01-29 17:40:48 +0300

    Перенес в абстрактный класс Encryption поле arrayAlphavit с латинским алфавитом

--- EncryptionLibrary/Abstractions/Encryption.cs
+++ EncryptionLibrary/Abstractions/Encryption.cs
@@ -11,6 +11,10 @@ namespace EncryptionLibrary.Abstractions
     {
         protected string? Line { get; set; }
 
+        protected readonly char[] arrayAlphavit = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+                                                'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                                                's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
         public abstract string Encrypt();
         public abstract string Decrypt();
     }
