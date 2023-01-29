37a4095648054d9b9550f5819f00ad148dcf0572
Igor Suvorov <suekandig@mail.ru>
2023-01-29 17:54:36 +0300

    Создал класс, определяющий шифрование Цезаря.

---
+++ EncryptionLibrary/Caesar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionLibrary
+{
+    public class Caesar : Encryption
+    {
+        public Caesar(string line) => Line = line;
+
+        public override string Encrypt() { return "There is no implementation yet"; }
+        public override string Decrypt() { return "There is no implementation yet"; }
+    }
+}
