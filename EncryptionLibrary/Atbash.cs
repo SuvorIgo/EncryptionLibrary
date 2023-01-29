05dc54a618075951e7af54bcc3f3d6039fec91b7
Igor Suvorov <suekandig@mail.ru>
2023-01-29 15:01:20 +0300

    Добавил readonly к полю masAlphavit

--- EncryptionLibrary/Atbash.cs
+++ EncryptionLibrary/Atbash.cs
@@ -10,7 +10,7 @@ namespace EncryptionLibrary
     {
         private string Line { get; set; }
 
-        private char[] masAlphavit = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+        private readonly char[] masAlphavit = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
                                        'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
                                        's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
