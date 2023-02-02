07e1f7b88741a9c7a8f435edc9804a3a5c360f70
Igor Suvorov <suekandig@mail.ru>
2023-02-02 13:55:13 +0300

    Добавил конструктор класса с двумя параметрами: string line, int step

--- EncryptionLibrary/Caesar.cs
+++ EncryptionLibrary/Caesar.cs
@@ -10,11 +10,17 @@ namespace EncryptionLibrary
     {
         public int Step { get; set; }
 
-        public Caesar(string line, string step)
+        public Caesar(string line)
             : base(line)
-        { 
+        {
             Step = 1;
-        } 
+        }
+
+        public Caesar(string line, int step)
+            : base(line)
+        {
+            Step = step;
+        }
 
         public override string Encrypt() { return "There is no implementation yet"; }
         public override string Decrypt() { return "There is no implementation yet"; }
