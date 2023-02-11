78ed7c2283c00678afc0d6636d4d5d103b0d453b
Igor Suvorov <suekandig@mail.ru>
2023-02-11 14:51:06 +0300

    Создал класс Vernam, являющийся описанием шифрования Вернама.

---
+++ EncryptionLibrary/Vernam.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionLibrary
+{
+    public sealed class Vernam : Encryption
+    {
+
+        public Vernam(string line) : base(line) { }
+
+        public override string Encrypt() { return "There is no implementation yet"; }
+        public override string Decrypt() { return "There is no implementation yet"; }
+    }
+}
