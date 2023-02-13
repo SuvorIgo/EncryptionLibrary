5a58be0473367a14db28a58cbfbaa66404d4f322
Igor Suvorov <suekandig@mail.ru>
2023-02-13 14:47:45 +0300

    Создал класс CodeWord, олицетворяющий шифрование клодового слова

---
+++ EncryptionLibrary/CodeWord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionLibrary
+{
+    public sealed class CodeWord : Encryption
+    {
+        CodeWord(string line) : base(line) { }
+
+        public override string Encrypt() => "There is no implementation yet";
+        public override string Decrypt() => "There is no implementation yet";
+
+    }
+}
