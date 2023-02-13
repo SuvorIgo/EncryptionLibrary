273ad8bb18656733608f533d1def36c266f5e8d7
Igor Suvorov <suekandig@mail.ru>
2023-02-13 15:21:58 +0300

    Подправил метод GetArrayLineKeyWithoutRepeat, добавив удаление пустых элементов в result[]

--- EncryptionLibrary/CodeWord.cs
+++ EncryptionLibrary/CodeWord.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace EncryptionLibrary
@@ -42,17 +43,32 @@ namespace EncryptionLibrary
 
         private char[] GetArrayLineKeyWithoutRepeat()
         {
-            var result = new char[];
+            Regex regex = new Regex(@"\s");
+
+            var result = new char[LineKey.Length];
 
             for (int i = 0; i < LineKey.Length; i++)
             {
-                if (result.Contains(LineKey[i]))
+                if (result.Contains(LineKey[i]) || regex.IsMatch(Convert.ToString(result[i])))
+                {
+                    Array.Resize(ref result, result.Length -1);
                     continue;
+                }
+
                 result[i] = LineKey[i];
             }
 
             return result;
         }
 
+        public void Display()
+        {
+            var result = GetArrayLineKeyWithoutRepeat();
+
+            foreach (var item in result)
+            {
+                Console.Write($"{Array.IndexOf(result, item)}{item} ");
+            }
+        }
     }
 }
